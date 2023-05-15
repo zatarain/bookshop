d5165986741ff48e04d308ae6caafc0de91faaaa
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-05-16 00:52:20 +0100

    BOOK-003: Adding health check to the API.
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"bookshop/configuration"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	log.Println("OK, go!")
+	defer log.Println("Good bye!")
+	configuration.Load()
+	server := gin.Default()
+	configuration.Setup(server)
+	server.Run()
 }
--- main_test.go
+++ main_test.go
@@ -2,20 +2,37 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os"
-	"strings"
 	"testing"
+
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(test *testing.T) {
+	assert := assert.New(test)
+	gin.SetMode(gin.TestMode)
+	recorder := httptest.NewRecorder()
 	var capture bytes.Buffer
 	log.SetOutput(&capture)
 	main()
+	request, exception := http.NewRequest(http.MethodHead, "/", nil)
+	assert.ErrorIs(exception, nil, "Check we don't get an error on the request.")
+
+	// Perform the request
+	server := gin.Default()
+	server.ServeHTTP(recorder, request)
+
+	// Check to see if the response was what you expected
+	assert.Equal(recorder.Code, http.StatusOK, "Checking expected status.")
+
 	log.SetOutput(os.Stderr)
 	actual := capture.String()
 	expected := "OK, go!"
-	if !strings.Contains(actual, expected) {
-		test.Errorf("Incorrect output, expected '%s' got '%s'", expected, actual)
-	}
+	assert.Contains(actual, expected, fmt.Sprintf("Incorrect output, expected '%s' got '%s'", expected, actual))
 }
