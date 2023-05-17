6b08ac3cd25ee9992f20bc1eff7ffe52ad6c7a94
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-05-17 17:41:13 +0100

    BOOK-004: Logging actual exception when we are not able to load environment
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- configuration/environment.go
+++ configuration/environment.go
@@ -13,6 +13,6 @@ func Load() {
 	log.Println("Loading environment file:", filename)
 	exception := godotenv.Load(filename)
 	if exception != nil {
-		log.Panic("Error loading environment variables file.")
+		log.Panic("Error loading environment variables file.", exception.Error())
 	}
 }
