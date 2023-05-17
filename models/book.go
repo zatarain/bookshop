97b584b49d3ff9d64f7d8551dc3d32d95bd633a3
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-05-17 12:30:17 +0100

    BOOK-004: Book model
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

---
+++ models/book.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Title     string    `json:"title"`
+	Author    string    `json:"author"`
+	Price     float32   `json:"price"`
+	Quantity  int       `json:"quantity"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
